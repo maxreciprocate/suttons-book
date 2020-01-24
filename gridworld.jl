3dd954f9c954fea8bbe60e43a83e7d3884c37df4
ogoremeni <yacc@tuta.io>
2020-01-24 13:31:40 +0200

    refactor(gridworld): strip

--- gridworld.jl
+++ gridworld.jl
@@ -2,7 +2,7 @@ using Test, Random, Plots
 
 function littleshufle(zs::AbstractVector)
     if length(zs) > 9
-        error("'tis a jest")
+        error("a jest")
     end
 
     y = BigInt(10)^rand(15:19)
@@ -14,14 +14,14 @@ function littleshufle(zs::AbstractVector)
     zs[idx]
 end
 
-function sampler(probs, what)
-    pdf = cumsum(probs)
+function sampler(probs, xs)
+    cdf = cumsum(probs)
 
     return function sample()
         U = rand()
-        idx = findfirst(x -> x > U || isnan(x), pdf)
+        idx = findfirst(x -> x > U || isnan(x), cdf)
 
-        what[idx]
+        xs[idx]
     end
 end
 
@@ -33,19 +33,22 @@ function pp(d::Dict)
     end
 end
 
-# ■ Value iteration with Q convergence
-N = 15
+# ■ Value iteration with Q-convergence
+N = 48
 kernel = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 block  = [[i, j] for i in 1:N, j in 1:N]
 
+# state actions
 A = map(x -> filter(c -> 1 <= c[1] <= N && 1 <= c[2] <= N, map(cell -> cell + x, kernel)), block)
-A[N, N] = []
-A[1, 1] = []
-
-V = zeros(Float64, N, N)
+# ways out
+A[N ÷ 2 + 1, N ÷ 2 + 1] = A[1, 1] = A[N, N] = A[1, N] = A[N, 1] = []
+# random policy
 P = map(x -> if isempty(x) nothing else rand(x) end, A)
+# empty values
+V = zeros(Float64, N, N)
 Q = map(x -> zeros(length(x)), A)
-γ = 0.9
+# discount
+γ = .97
 
 while true
     Δ = 1
@@ -57,20 +60,23 @@ while true
 
             _V = V[i]
 
-            V[i] = -1.0 + γ * V[P[i]...]
+            V[i] = -1 + γ * V[P[i]...]
 
             Δ = max(Δ, abs(_V - V[i]))
         end
 
     end
 
+    # check convergence
     _Q = deepcopy(Q)
 
     for i in CartesianIndices(A)
         isempty(A[i]) && continue
 
+        # recompute Q-values
         Q[i] = map(s -> -1.0 + γ * V[s...], A[i])
 
+        # update policy
         P[i] = A[i][argmax(Q[i])]
     end
 
@@ -81,61 +87,7 @@ while true
 end
 
 display(P)
-display(V)
 display(Q)
-
-heatmap(V, c=:matter, yflip=true, aspect_ratio=1)
-
-# ■ Q iteration
-N = 15
-kernel = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-block  = [[i, j] for i in 1:N, j in 1:N]
-
-A = map(x -> filter(c -> 1 <= c[1] <= N && 1 <= c[2] <= N, map(cell -> cell + x, kernel)), block)
-A[N, N] = []
-A[1, 1] = []
-
-P = map(x -> if isempty(x) nothing else rand(x) end, A)
-Q = map(x -> zeros(length(x)), A)
-γ = 0.9
-
-map(s_ -> -1.0 + γ * maximum(Q[s_...]), A[3, 3])
-
-while true
-    Δ = 1
-    while Δ > 0.1
-        Δ = 0
-
-        for i in CartesianIndices(A)
-            if isempty(A[i]) continue end
-
-            _Q = deepcopy(Q)
-            Q[i] = map(s_ -> -1.0 + γ * maximum(Q[s_...]), A[i])
-
-            V[i] = -1.0 + γ * V[P[i]...]
-
-            Δ = max(Δ, abs(_V - V[i]))
-        end
-
-    end
-
-    _Q = deepcopy(Q)
-
-    for i in CartesianIndices(A)
-        if isempty(A[i]) continue end
-
-        Q[i] = map(s -> -1.0 + γ * V[s...], A[i])
-
-        P[i] = A[i][argmax(Q[i])]
-    end
-
-    _Q == Q && break
-
-    @show maximum(Q - _Q)
-end
-
-display(P)
 display(V)
-display(Q)
 
 heatmap(V, c=:matter, yflip=true, aspect_ratio=1)
